4a7cd7355c92225bd88a7072dcf173411fe1f297
Zhenya <evgeniikapchik@gmail.com>
2021-07-05 18:17:08 +0300

    add:first subtask

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,26 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        if ( array[0] < 0 || array[1] <= 0 || array.size < 2 || array.isEmpty())
+            return null
+
+        val a = array[0]
+        val b = array[1]
+
+        for (i in 0..b) {
+            if (factorial(b) == a * factorial(b - i) * factorial(i))
+                return i
+        }
+        return null
+    }
+
+    private fun factorial(n: Int): Int {
+        var factorial = 1
+        for (i in 1..n) {
+            factorial *= i
+        }
+        return factorial
     }
 }
