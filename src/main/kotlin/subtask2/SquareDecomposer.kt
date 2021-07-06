e3745fbcbf9b471f60e5544a65f8d1906eafa803
Zhenya <evgeniikapchik@gmail.com>
2021-07-06 18:18:06 +0300

    try 2 task

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,19 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
+
+        if (number <= 0) return null
+        /*val sqrtNumber = sqrt(number.toDouble())*/
+        var j = 0
+
+       /* for (i in sqrt(number.toDouble())..1) {
+            j = sqrt(number.toDouble()) - i * i
+        }*/
+
         throw NotImplementedError("Not implemented")
     }
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,8 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
+
+
         throw NotImplementedError("Not implemented")
     }
 }
