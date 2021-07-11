bb5cd75c31fa555b38bf06f58c06270db4cb838a
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-07-11 10:25:03 +0300

    3rd task

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,54 +4,42 @@ class TelephoneFinder {
 
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
 
-        if (number[0] == '-') return null
+        val listNumbers = numberMap()
 
+        val result = mutableListOf<String>()
         val numChars = number.toCharArray()
-        for (i in 0..numChars.size) {
-            /*  when (numChars[i]) {
-                '1' -> //TODO
-                    '2'
-                ->//TODO
+        for (i in numChars.indices) {
+            if (!numChars[i].isDigit())
+                return null
 
-                    '3'
-                ->  //TODO
-                    //  '7'                ->
-                    '8' tyty
-                            '4'
-                -> //TODO
-                    '5'
-                -> yg
-                '6' -> gfgf
-                ->
-                    '9'
-                ->
-                    '0'
-                ->
-                else -> return null
-
-            }
-
-        }*/
-
-
-            throw NotImplementedError("Not implemented")
+            val neighbourNumber = numChars.clone()
+            val neighbours: List<Int>? = listNumbers[Character.getNumericValue(neighbourNumber[i])]
+            changeNumber(i, result, neighbours, neighbourNumber)
         }
+        return result.toTypedArray()
+    }
 
-        /* private fun changeNumber(pos: Int, nums: Array<Char>, number: String): ArrayList<String> {
+    private fun changeNumber(pos: Int, result: MutableList<String>, neighbours: List<Int>?, number: CharArray) {
+        neighbours?.let {
+            for (j in it.indices) {
+                number[pos] = '0' + it[j]
+                result.add(number.joinToString(separator = ""))
+            }
+        }
+    }
 
-    }*/
-
-        val totalMap = mapOf(
-            '0' to arrayListOf('8'),
-            '1' to arrayListOf('2', '4'),
-            '2' to arrayListOf('1', '3', '5'),
-            '3' to arrayListOf('2', '6'),
-            '4' to arrayListOf('1', '5', '7'),
-            '5' to arrayListOf('2', '4', '6', '8'),
-            '6' to arrayListOf('3', '5', '9'),
-            '7' to arrayListOf('4', '8'),
-            '8' to arrayListOf('5', '7', '9', '0'),
-            '9' to arrayListOf('6', '8')
+    private fun numberMap(): Map<Int, List<Int>> {
+        return mapOf(
+            0 to listOf(8),
+            1 to listOf(2, 4),
+            2 to listOf(1, 3, 5),
+            3 to listOf(2, 6),
+            4 to listOf(1, 5, 7),
+            5 to listOf(2, 4, 6, 8),
+            6 to listOf(3, 5, 9),
+            7 to listOf(4, 8),
+            8 to listOf(5, 7, 9, 0),
+            9 to listOf(6, 8)
         )
     }
 }
